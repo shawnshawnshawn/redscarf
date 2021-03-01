d4b64956f91a42165749baeac7ff80f4afb2ee50
白也 <858261367@qq.com>
2021-03-01 15:59:37 +0800

    init resp

---
+++ api/src/main/java/com/baiye/redscarf/api/RedscarfApplication.java
@@ -0,0 +1,13 @@
+package com.baiye.redscarf.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedscarfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedscarfApplication.class, args);
+    }
+
+}
