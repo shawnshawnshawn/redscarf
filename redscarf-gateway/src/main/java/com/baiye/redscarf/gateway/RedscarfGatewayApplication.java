7f2c586448c494d41e4089ade7ea89f4b3cc6143
白也 <858261367@qq.com>
2021-03-05 14:59:24 +0800

    update gateway modules

--- redscarf-gateway/src/main/java/com/baiye/redscarf/gateway/RedscarfGatewayApplication.java
+++ redscarf-gateway/src/main/java/com/baiye/redscarf/gateway/RedscarfGatewayApplication.java
@@ -2,15 +2,11 @@ package com.baiye.redscarf.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author baiye
  * @date 2021/3/3 6:26 下午
  **/
-@EnableDiscoveryClient
-@EnableFeignClients
 @SpringBootApplication
 public class RedscarfGatewayApplication {
     public static void main(String[] args) {
