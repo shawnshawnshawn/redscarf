c70b980ab47e2239813d8b0ecaacf547e2f7898b
白也 <858261367@qq.com>
2021-08-20 10:53:04 +0800

    update

---
+++ redscarf-gateway/src/main/java/com/baiye/redscarf/gateway/cros/WebFilter.java
@@ -0,0 +1,13 @@
+package com.baiye.redscarf.gateway.cros;
+
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author baiye
+ * @since 2021/8/19 4:15 下午
+ **/
+@Configuration
+public class WebFilter {
+
+}
